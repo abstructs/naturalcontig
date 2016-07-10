7be6e18b11f885295436f6867bf2840b6f5fd023
polygoning <polygoning@gmail.com>
2016-07-09 20:58:28 -0400

    init repo

---
+++ script.js
@@ -0,0 +1,13 @@
+var createDataSet = function() {
+  var data = [];
+  for (var i = 0; i < 75; i++) {
+    var rand = Math.floor((Math.random() * 100) + 10)
+    data.push(rand);
+  }
+  return data
+}
+
+$(function(){
+  var data = getData();
+  console.log(data)
+});
